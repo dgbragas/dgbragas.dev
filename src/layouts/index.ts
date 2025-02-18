e414739b6801a0af440ed2adaf1bccb450151ef8
Dg <work@dgbragas.com>
2025-02-17 22:28:24 -0300

    refactor: standardize layout file names and exports

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,2 +1,4 @@
-export * from './BaseLayout.astro';
-export * from './PostLayout.astro';
+import BaseLayout from './Base.layout.astro';
+import PostLayout from './Post.layout.astro';
+
+export { BaseLayout, PostLayout };

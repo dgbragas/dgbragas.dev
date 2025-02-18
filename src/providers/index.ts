58b090485e8db31bdbe72bff83b53d8d2a2b5ed9
Dg <work@dgbragas.com>
2025-02-17 22:28:42 -0300

    refactor: standardize providers file names and exports

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,2 +1,4 @@
-export * from './LayoutProvider.astro';
-export * from './StylesProvider.astro';
+import LayoutProvider from './Layout.provider.astro';
+import StylesProvider from './Styles.provider.astro';
+
+export { LayoutProvider, StylesProvider };

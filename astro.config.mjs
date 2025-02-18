5925391c03e188b68003c8a30e6b4ac55d6c9e1e
Dg <work@dgbragas.com>
2025-02-17 22:10:50 -0300

    chore: configure vite css and resolve

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,21 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import path from 'path';
 
 export default defineConfig({
   integrations: [react()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/styles/tokens.scss" as *;',
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
 });

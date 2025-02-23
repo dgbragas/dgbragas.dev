ce5c7fcf7c22f2a613978825b4c7af04601ed9f4
Dg <work@dgbragas.com>
2025-02-22 21:50:54 -0300

    feat: create Radio component

---
+++ src/components/lib/Radio/Radio.component.tsx
@@ -0,0 +1,33 @@
+import { RadioGroup } from 'radix-ui';
+import './Radio.styles.scss';
+
+type RadioProps = {
+  ariaLabel: string;
+  defaultValue: string;
+  values: Array<{
+    id: string;
+    label: string;
+    value: string;
+  }>;
+};
+
+const Radio = ({ ariaLabel, defaultValue, values }: RadioProps) => (
+  <RadioGroup.Root
+    aria-label={ariaLabel}
+    className='radio'
+    defaultValue={defaultValue}
+  >
+    {values.map(({ id, label, value }) => (
+      <div className='radio__group'>
+        <RadioGroup.Item className='radio__group__item' id={id} value={value}>
+          <RadioGroup.Indicator className='radio__indicator' />
+        </RadioGroup.Item>
+        <label className='radio__group__label' htmlFor={id}>
+          {label}
+        </label>
+      </div>
+    ))}
+  </RadioGroup.Root>
+);
+
+export { Radio };

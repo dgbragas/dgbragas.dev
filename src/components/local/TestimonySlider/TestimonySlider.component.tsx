323500f2c7ab322dae4e8115729ac83b3ec92ae5
Dg <work@dgbragas.com>
2025-02-26 23:41:36 -0300

    feat: add testimony-slider structure

--- src/components/local/TestimonySlider/TestimonySlider.component.tsx
+++ src/components/local/TestimonySlider/TestimonySlider.component.tsx
@@ -5,9 +5,30 @@ import useEmblaCarousel from 'embla-carousel-react';
 import QuoteIcon from '@/assets/icons/quote.svg?raw';
 
 import './TestimonySlider.styles.scss';
+import { useSliderDot, type EmblaType } from './TestimonySlider.hook';
+
+type TestimonySliderDot = {
+  onClick: VoidFunction;
+  selected: boolean;
+};
+
+const TestimonySliderDot = ({ onClick, selected }: TestimonySliderDot) => (
+  <button
+    className={
+      selected
+        ? `testimony-slider__navigation__dot--selected`
+        : `testimony-slider__navigation__dot`
+    }
+    onClick={onClick}
+    type='button'
+  />
+);
 
 const TestimonySlider = () => {
-  const [emblaRef] = useEmblaCarousel();
+  const [emblaRef, emblaAPI] = useEmblaCarousel();
+  const { currentIndex, onDotButtonClick, scrollSnaps } = useSliderDot(
+    emblaAPI as EmblaType,
+  );
 
   return (
     <div className='testimony-slider' ref={emblaRef}>
@@ -23,6 +44,27 @@ const TestimonySlider = () => {
             <small>Avaliação pós-entrega</small>
           </div>
         </div>
+        <div className='testimony-slider__slide'>
+          <span aria-hidden dangerouslySetInnerHTML={{ __html: QuoteIcon }} />
+          <div className='testimony-slider__slide__content'>
+            <h3>
+              Gostei muito do novo portfólio, de verdade. Achei incrível o
+              trabalho, gostei muito. Recomendo muito, ficou monstro!
+            </h3>
+            <span>Danilo</span>
+            <small>Avaliação pós-entrega</small>
+          </div>
+        </div>
+      </div>
+
+      <div className='testimony-slider__navigation'>
+        {scrollSnaps.map((_, index) => (
+          <TestimonySliderDot
+            key={index}
+            selected={currentIndex === index}
+            onClick={() => onDotButtonClick(index)}
+          />
+        ))}
       </div>
     </div>
   );

e5e5ee2480dcfc1a92372acadef148effd239d5d
Dg <work@dgbragas.com>
2025-03-02 23:28:21 -0300

    refactor: improve InputText props structure

--- src/components/lib/InputText/InputText.component.tsx
+++ src/components/lib/InputText/InputText.component.tsx
@@ -1,31 +1,47 @@
+import { Text } from '@/components/lib';
 import './InputText.styles.scss';
 
 type InputTextProps = {
   autoComplete?: React.HTMLInputAutoCompleteAttribute;
+  error?: boolean;
+  errorMessage?: string;
   id: string;
-  type?: 'number' | 'text';
+  label: string;
   required?: boolean;
+  type?: 'number' | 'text';
 };
 
 const InputText = ({
   autoComplete,
+  errorMessage,
   id,
+  label,
+  error = false,
   required = false,
   type = 'text',
 }: InputTextProps) => (
   <div className='input-text__group'>
     <input
-      aria-required={required}
       autoComplete={autoComplete}
+      aria-invalid={error}
+      aria-labelledby={id}
+      aria-required={required}
       className='input-text'
       id={id}
       placeholder=''
       required={required}
       type={type}
     />
-    <label className='input-text__label' htmlFor={id}>
-      Label
+
+    <label className='input-text__label' htmlFor={id} id={id}>
+      {label}
     </label>
+
+    {error && (
+      <Text className='input-text__error' element='small' variant='small'>
+        {errorMessage}
+      </Text>
+    )}
   </div>
 );
 

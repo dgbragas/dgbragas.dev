920513c5ec3538aedc0642b68435f68e43ea9e44
Dg <work@dgbragas.com>
2025-03-10 23:48:35 -0300

    feat: create CardPortfolio structure

--- src/components/lib/CardPortfolio/CardPortfolio.component.tsx
+++ src/components/lib/CardPortfolio/CardPortfolio.component.tsx
@@ -1,5 +1,60 @@
+import { Tag } from '../Tag';
+import { Text } from '../Text';
 import './CardPortfolio.styles.scss';
 
-const CardPortfolio = () => <></>;
+type CardPortfolioProps = {
+  description: string;
+  image: {
+    alt: string;
+    src: string;
+  };
+  labels: string[];
+  variant?: 'extended' | 'overlay';
+  title: string;
+  url: {
+    href: string;
+    target: '_blank' | '_self';
+  };
+};
+
+const CardPortfolio = ({
+  description,
+  image,
+  labels,
+  title,
+  url,
+  variant = 'overlay',
+}: CardPortfolioProps) => (
+  <a
+    className={`card-portfolio card-portfolio--${variant}`}
+    href={url.href}
+    target={url.target}
+  >
+    <img src={image.src} alt={image.alt} />
+    <article className='card-portfolio__container'>
+      <div className='card-portfolio__labels'>
+        {labels.map(label => (
+          <Tag label={label} />
+        ))}
+      </div>
+      <header className='card-portfolio__header'>
+        <Text
+          element='h3'
+          variant={variant === 'extended' ? 'heading-4' : 'intro-highlight'}
+        >
+          {title}
+        </Text>
+      </header>
+      <Text
+        element='p'
+        variant={variant === 'extended' ? 'extended-medium' : 'body'}
+      >
+        {description}
+      </Text>
+    </article>
+
+    {variant === 'overlay' && <div className='card-portfolio__overlay' />}
+  </a>
+);
 
 export { CardPortfolio };

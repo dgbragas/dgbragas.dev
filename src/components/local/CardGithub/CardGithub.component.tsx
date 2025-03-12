2d14a4b554d269f3448fbeade468f41b4f30cec6
Dg <work@dgbragas.com>
2025-03-11 23:40:12 -0300

    feat: create CardGithub structure

--- src/components/local/CardGithub/CardGithub.component.tsx
+++ src/components/local/CardGithub/CardGithub.component.tsx
@@ -1,5 +1,26 @@
-import './CardGithub.styles.scss';
+import GithubIcon from '@/assets/icons/github.svg?raw';
 
-const CardGithub = () => <></>;
+import './CardGithub.styles.scss';
+import { Text } from '@/components/lib';
+
+type CardGithubProps = {
+  description: string;
+  repoUrl: string;
+  title: string;
+};
+
+const CardGithub = ({ description, repoUrl, title }: CardGithubProps) => (
+  <a className='card-github' href={repoUrl} target='_blank'>
+    <div className='card-github__header'>
+      <span aria-hidden dangerouslySetInnerHTML={{ __html: GithubIcon }} />
+      <header>
+        <Text element='h3' variant='intro-highlight'>
+          {title}
+        </Text>
+      </header>
+    </div>
+    <Text element='p'>{description}</Text>
+  </a>
+);
 
 export { CardGithub };

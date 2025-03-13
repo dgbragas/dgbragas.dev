413171ebea2e8f618de2fb15d11d123ca2d37b16
Dg <work@dgbragas.com>
2025-03-12 23:34:22 -0300

    feat: reestructure CardHow

--- src/components/local/CardHow/CardHow.component.tsx
+++ src/components/local/CardHow/CardHow.component.tsx
@@ -1,5 +1,27 @@
+import { Text } from '@/components/lib';
+
 import './CardHow.styles.scss';
 
-const CardHow = () => <></>;
+type CardHowProps = {
+  description: string;
+  icon: string;
+  title: string;
+};
+
+const CardHow = ({ description, icon, title }: CardHowProps) => (
+  <div className='card-how'>
+    <span dangerouslySetInnerHTML={{ __html: icon }} />
+    <div className='card-how__content'>
+      <header>
+        <Text element='h3' variant='heading-4'>
+          {title}
+        </Text>
+      </header>
+      <Text element='p' variant='extended-medium'>
+        {description}
+      </Text>
+    </div>
+  </div>
+);
 
 export { CardHow };

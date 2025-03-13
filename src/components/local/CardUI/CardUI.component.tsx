ccdcf1bed434e4225f1bfac642d111b3643923df
Dg <work@dgbragas.com>
2025-03-13 00:13:53 -0300

    feat: create CardUI component structure

--- src/components/local/CardUI/CardUI.component.tsx
+++ src/components/local/CardUI/CardUI.component.tsx
@@ -1,5 +1,42 @@
+import { Tag, Text } from '@/components/lib';
+
 import './CardUI.styles.scss';
 
-const CardUI = () => <></>;
+type CardUIProps = {
+  description: string;
+  image: {
+    alt: string;
+    src: string;
+  };
+  labels: string[];
+  title: string;
+  url: string;
+};
+
+const CardUI = ({ description, image, labels, title, url }: CardUIProps) => (
+  <a className='card-ui' href={url} target='_blank'>
+    <article className='card-ui__article'>
+      <img alt={image.alt} src={image.src} />
+
+      <div className='card-ui__content'>
+        <div className='card-ui__content__labels'>
+          {labels.map(label => (
+            <Tag label={label} />
+          ))}
+        </div>
+
+        <div className='card-ui__content__presentation'>
+          <header>
+            <Text element='h2' variant='intro-highlight'>
+              {title}
+            </Text>
+          </header>
+
+          <Text element='p'>{description}</Text>
+        </div>
+      </div>
+    </article>
+  </a>
+);
 
 export { CardUI };
--- src/components/local/index.ts
+++ src/components/local/index.ts
@@ -1,4 +1,5 @@
 export * from './CardGitHub';
 export * from './CardHow';
+export * from './CardUI';
 export * from './CardWorkflow';
 export * from './TestimonySlider';

42931760cec5aa9f41551000523275e30c1e274c
Dg <work@dgbragas.com>
2025-03-22 23:01:34 -0300

    feat: create ShareDropdown component

---
+++ src/components/lib/ShareDropdown/ShareDropdown.component.tsx
@@ -0,0 +1,51 @@
+import { DropdownMenu as RUIDropdown } from 'radix-ui';
+
+import './ShareDropdown.styles.scss';
+import { Text } from '@/components/lib';
+
+type ShareDropdownTypes = {
+  className?: string;
+  items: Array<{
+    command: {
+      keys: string[];
+      display: string;
+    };
+    href: string;
+    text: string;
+  }>;
+  trigger: {
+    label: string;
+    text: string;
+  };
+};
+
+const ShareDropdown = ({ items, trigger }: ShareDropdownTypes) => (
+  <RUIDropdown.Root>
+    <RUIDropdown.Trigger asChild>
+      <button aria-label={trigger.label} className='share-dropdown__trigger'>
+        {trigger.text}
+      </button>
+    </RUIDropdown.Trigger>
+
+    <RUIDropdown.Portal>
+      <RUIDropdown.Content className='share-dropdown'>
+        {items.map(item => (
+          <RUIDropdown.Item
+            className='share-dropdown__item'
+            asChild
+            key={item.text}
+          >
+            <a href={item.href} target='_blank'>
+              <Text variant='small'>{item.text}</Text>
+              <Text element='small' variant='caption'>
+                {item.command.display}
+              </Text>
+            </a>
+          </RUIDropdown.Item>
+        ))}
+      </RUIDropdown.Content>
+    </RUIDropdown.Portal>
+  </RUIDropdown.Root>
+);
+
+export { ShareDropdown, type ShareDropdownTypes };

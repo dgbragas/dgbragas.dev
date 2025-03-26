d447e4b8d8c2d4633dbaa6c642eb6808c3f6cf0b
Dg <work@dgbragas.com>
2025-03-26 20:18:23 -0300

    refactor: split posts api in 2 uniq functions

--- src/api/posts.api.ts
+++ src/api/posts.api.ts
@@ -4,6 +4,7 @@ import { baseUrl } from '@/constants/baseUrl.constants';
 
 type Post = {
   content: string;
+  documentId: string;
   intro: Array<{
     children: Array<{
       text: string;
@@ -16,6 +17,9 @@ type Post = {
   title: string;
 };
 
+const formatDate = (date: Date) =>
+  new Intl.DateTimeFormat('pt-BR').format(date);
+
 const getPosts = async () => {
   const response = await fetch(`${baseUrl.server}/posts`);
   const json = await response.json();
@@ -23,21 +27,39 @@ const getPosts = async () => {
   const rawPosts = json.data as Post[];
 
   const formattedData = rawPosts.map(post => {
-    const date = new Date(post.publishedAt);
+    const { readTime, tags, title } = post;
 
-    const content = marked(post.content);
     const intro = marked(post.intro[0].children[0].text);
-    const publishedAt = new Intl.DateTimeFormat('pt-BR').format(date);
+    const publishedAt = formatDate(new Date(post.publishedAt));
 
     return {
-      ...post,
-      content,
       intro,
       publishedAt,
+      readTime,
+      tags,
+      title,
     };
   });
 
   return formattedData;
 };
 
-export { getPosts };
+const findOnePost = async (documentId: string) => {
+  const response = await fetch(`${baseUrl.server}/posts/${documentId}`);
+  const json = await response.json();
+
+  const post = json.data as Post;
+
+  const content = marked(post.content);
+  const intro = marked(post.intro[0].children[0].text);
+  const publishedAt = formatDate(new Date(post.publishedAt));
+
+  return {
+    ...post,
+    content,
+    intro,
+    publishedAt,
+  };
+};
+
+export { findOnePost, getPosts };

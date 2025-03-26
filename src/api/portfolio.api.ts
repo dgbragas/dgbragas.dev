90d3e1b8d4f503744f82c36afafc150bddbc74f1
Dg <work@dgbragas.com>
2025-03-26 20:00:18 -0300

    feat: create portfolio fetch api with formatted values

---
+++ src/api/portfolio.api.ts
@@ -0,0 +1,41 @@
+import { baseUrl } from '@/constants/baseUrl.constants';
+
+type PortfolioPost = {
+  description: string;
+  id: number;
+  image: {
+    alternativeText: string;
+    caption: string;
+    url: string;
+  };
+  private: boolean;
+  tags: string[];
+};
+
+const getPortfolioPosts = async () => {
+  const response = await fetch(`${baseUrl.server}/portfolios?populate=image`);
+  const json = await response.json();
+
+  const rawPosts = json.data as PortfolioPost[];
+
+  const { privatePosts, publicPosts } = rawPosts.reduce(
+    (accumulator, currentValue) => {
+      if (currentValue.private) {
+        accumulator.privatePosts.push(currentValue);
+      } else {
+        accumulator.publicPosts.push(currentValue);
+      }
+
+      return accumulator;
+    },
+    {
+      privatePosts: [] as PortfolioPost[],
+      publicPosts: [] as PortfolioPost[],
+    },
+  );
+  const tags = [...new Set(rawPosts.flatMap(({ tags }) => tags))];
+
+  return { privatePosts, publicPosts, tags };
+};
+
+export { getPortfolioPosts };

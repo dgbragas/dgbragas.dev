749d94e9f4436bfc888bb9325163de9abb80d571
Dg <work@dgbragas.com>
2025-03-30 23:34:34 -0300

    feat: add github url in socials constant

--- src/constants/socials.constants.ts
+++ src/constants/socials.constants.ts
@@ -1,6 +1,7 @@
 const socials = {
   behance: 'https://www.behance.net/dgbragas',
   dribbble: 'http://www.dribbble.com/dgbragas',
+  github: 'https://www.github.com/dgbragas',
   instagram: 'http://www.instagram.com/dgbragas.dev',
   linkedIn: 'https://www.linkedin.com/in/dgbragas/',
   mail: 'mailto:work@dgbragas.com?subject=%5BJob%5D%20-%20Service%20from%20dgbragas.dev&body=Ol%C3%A1%21%20Vi%20o%20seu%20portf%C3%B3lio%20e%20fiquei%20interessado%20em%20contratar%20os%20seus%20servi%C3%A7os.%0A%0AEu%20preciso%20de%3A%20%28descreva%20a%20sua%20necessidade%29%0AEm%20um%20prazo%20de%20at%C3%A9%3A%20%28dias%2Fsemanas%2Fmeses%29%0ACom%20um%20budget%20em%20torno%20de%3A%20R%24%0A%0A',

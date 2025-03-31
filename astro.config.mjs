133d05059388644820b48b39520c2a484eb8aac2
Dg <work@dgbragas.com>
2025-03-31 02:15:19 -0300

    feat: configure vercel adapter in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 
 import path from 'path';
 
 export default defineConfig({
   // INFO: remove after links filter feature
   output: 'server',
+  adapter: vercel(),
   integrations: [react()],
   vite: {
     css: {

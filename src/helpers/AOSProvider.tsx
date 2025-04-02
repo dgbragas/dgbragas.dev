68be4ff3a6b1243e52aa2ebae21fe75321843789
Dg <work@dgbragas.com>
2025-04-02 00:52:14 -0300

    feat: create AOSProvider helper

---
+++ src/helpers/AOSProvider.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+import AOS from 'aos';
+
+import 'aos/dist/aos.css';
+
+function AOSProvider() {
+  React.useEffect(() => {
+    AOS.init({
+      duration: 800,
+    });
+
+    setTimeout(() => AOS.refreshHard(), 500);
+  }, []);
+
+  return null;
+}
+
+export { AOSProvider };
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,1 +1,2 @@
+export * from './AOSProvider';
 export * from './SmoothScroll';
